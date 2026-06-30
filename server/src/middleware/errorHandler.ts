733efd90e7469096744d5bd18456d18a47a4c4bf
Manikanta Sarma Ganti <mganti@issi-software.com>
2026-06-30 14:09:32 -0400

    feat/TF-105-error-handling

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,17 +1,50 @@
-const errorHandler = async (error, req, res, _next) => {
-  const statusCode = error.statusCode || 500;
-  let message;
-  if (error.isOperational) {
-    message = error.message;
-  } else {
-    message = "Something went wrong";
-  }
+import { ErrorCodes } from "../../../shared/src/ErrorCodes";
 
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../lib/AppError";
+
+const errorHandler = async (
+  error: AppError,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  let statusCode = error.statusCode || 500;
+  let message = error.message || "Something Went Wrong";
+  let code = error.code || ErrorCodes.INTERNAL;
+
+  //handling prisma errors
+  if (error instanceof PrismaClientKnownRequestError) {
+    switch (error.code) {
+      case "P2002": {
+        const field = (error.meta?.target as string[])?.join(", ");
+        statusCode = 409;
+        message = `${field} already exists`;
+        code = ErrorCodes.CONFLICT;
+        break;
+      }
+
+      case "P2025": {
+        statusCode = 404;
+        message = "Record not found";
+        code = ErrorCodes.NOT_FOUND;
+        break;
+      }
+
+      default: {
+        statusCode = 500;
+        message = "Something Went Wrong";
+        code = ErrorCodes.INTERNAL;
+      }
+    }
+  }
+  console.error("Error", error);
   return res.status(statusCode).json({
     success: false,
     error: {
       message,
-      code: "INTERNAL_SERVER_ERROR",
+      code,
       statusCode,
     },
   });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 // import { prisma } from "./lib/prisma";
+import { AppError } from "./lib/AppError";
+import errorHandler from "./middleware/errorHandler";
+import rateLimit from "express-rate-limit";
+import { Request, Response, NextFunction } from "express";
+import { ErrorCodes } from "../../shared/src/ErrorCodes";
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  limit: 5,
+  handler: function (req: Request, res: Response, next: NextFunction) {
+    next(
+      new AppError(
+        "Too many requests, please try again later",
+        429,
+        ErrorCodes.TOO_MANY_REQUESTS,
+      ),
+    );
+  },
+});
 
 dotenv.config();
 
@@ -8,10 +27,17 @@ const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
+app.use(limiter);
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.json({ message: "TaskFlow API running" });
 });
+
+app.use((_req, _res, _next) => {
+  throw new AppError("Requested URL Not Found", 404, "NOT_FOUND");
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
